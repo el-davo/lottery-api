ff40bb923e4b2ad662986dff6df9cce77f9f1b65
ahernd2 <david.ahern@emc.com>
2017-09-16 08:17:30 +0100

    Disabling endpoints

--- common/models/ticket.js
+++ common/models/ticket.js
@@ -14,6 +14,51 @@ module.exports = Ticket => {
     let {totalLines, id, lines: {create}} = ctx.instance;
     let lines = times(totalLines, () => ({ticketId: id}));
 
-    create(lines, next);
+    create(lines, (err, lines) => {
+      console.log(lines);
+      next(err, lines);
+    });
   });
+
+  /**
+   * Disable remote api methods
+   */
+  Ticket.disableRemoteMethodByName('deleteById');
+  Ticket.disableRemoteMethodByName('upsert');
+  Ticket.disableRemoteMethodByName('upsertWithWhere');
+  Ticket.disableRemoteMethodByName('replaceOrCreate');
+  Ticket.disableRemoteMethodByName('replace');
+  Ticket.disableRemoteMethodByName('replaceById');
+  Ticket.disableRemoteMethodByName('updateAll');
+  Ticket.disableRemoteMethodByName('prototype.updateAttributes');
+
+  Ticket.disableRemoteMethodByName('findOne');
+  Ticket.disableRemoteMethodByName('exists');
+
+  Ticket.disableRemoteMethodByName('createChangeStream');
+  Ticket.disableRemoteMethodByName('count');
+
+  // Line Relations
+  Ticket.disableRemoteMethodByName('prototype.__count__lines');
+  Ticket.disableRemoteMethodByName('prototype.__get__lines');
+  Ticket.disableRemoteMethodByName('prototype.__upsert__lines');
+  Ticket.disableRemoteMethodByName('prototype.__delete__lines');
+  Ticket.disableRemoteMethodByName('prototype.__destroyById__lines');
+  Ticket.disableRemoteMethodByName('prototype.__updateById__lines');
+  Ticket.disableRemoteMethodByName('prototype.__findOne__lines');
+  Ticket.disableRemoteMethodByName('prototype.__findById__lines');
+
+  // Number Relations
+  Ticket.disableRemoteMethodByName('prototype.__count__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__get__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__destroyById__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__delete__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__updateById__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__create__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__updateById__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__findOne__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__find__numbers');
+  Ticket.disableRemoteMethodByName('prototype.__findById__numbers');
+
+  Ticket.disableRemoteMethodByName('prototype.__destroyById__numbers_rel');
 };
